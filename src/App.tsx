d94ba093844997493ae44c1535952b400355ab31
Bressan48 <bernardobressandemattos@gmail.com>
2024-11-17 15:06:06 -0300

    implementa desestruturação de objetos

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ function App()
   return (
     <>
     <h1>Hello, Warudo!</h1>
+    <Disciplina codigo='DCC202' nome='Desenvolvimento Web I' />
+    <Disciplina codigo='DCC206' nome='Desenvolvimento Web II' />
     <Disciplina codigo='DCC207' nome='Desenvolvimento Web Front-End' />
     </>
   );
